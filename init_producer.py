06aced052c44f68c64d6b0315024d5e44dddefae
Tom <tom.b.montgomery@gmail.com>
2024-10-30 20:50:09 +0200

    initial commit

---
+++ init_producer.py
@@ -0,0 +1,9 @@
+# init_producer.py
+from flask import Flask
+
+# Create a Flask instance
+app = Flask(__name__)
+
+# Load Flask configurations from config.py
+app.secret_key = app.config['SECRET_KEY']
+app.config.from_object("config")
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Shutting down 
+Kill the process with `Ctrl+C`
+
+Kill the process using `kill -9 $(lsof -t -i :5000)`
